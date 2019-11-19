c3378deca583c5f929f060ef8e445abb235b2fd0
pmnhatit <pmn.99.20@gmail.com>
2019-11-19 17:16:40 +0700

    fix layout and query

--- routes/category.js
+++ routes/category.js
@@ -1,21 +1,21 @@
 var express = require('express');
 const mongoose = require('mongoose');
-var db=mongoose.connection;
+var db = mongoose.connection;
 var router = express.Router();
 
 
-  //create schame
+//create schame
 var categoryschema = new mongoose.Schema({
   name: String,
   price: String,
   category: String,
   details: String,
-  img:String 
+  img: String
 },
-{
-  collection:'category'
+  {
+    collection: 'category'
 
-});
+  });
 
 //load data
 // const cate = db.useDb("mydb").model("cate",categoryschema);
@@ -45,30 +45,33 @@ var categoryschema = new mongoose.Schema({
 
 
 //load data
-const cate = db.useDb("mydb").model("cate",categoryschema);
-router.get('/', async (req, res) => { 
+const cate = db.useDb("mydb").model("cate", categoryschema);
+router.get('/', async (req, res) => {
   let value
-  if(typeof req.query.q ==='undefined')
-  {
+  if (typeof req.query.q === 'undefined') {
     try {
-      value=await cate.find({})
+      value = await cate.find({})
     } catch (error) {
       console.log("loi roi");
       res.render('error');
 
     }
     console.log("get data successfully");
-    res.render('category', {title:'category', list: value })  
+    res.render('category', { title: 'category', list: value })
   }
-else{
-  try {
-    value=await cate.find({'categoryid':req.query.q})
-  } catch (error) {
-    console.log("loi roi");
-    res.render('error');
-  }
+  else {
+    try {
+      value = await cate.find({ 'categoryid': req.query.q })
+    } catch (error) {
+      console.log("loi roi");
+      res.render('error');
+    }
     console.log("query la:" + req.query.q);
-      res.render('category', {title:value[0].category, list: value })
+    if (value && value.length)
+      res.render('category', { title: value[0].category, list: value })
+    else
+      res.render('category', { list: value })
+
   }
 })
 
@@ -82,5 +85,5 @@ else{
 // })
 // })
 
-  module.exports = router;
+module.exports = router;
 
